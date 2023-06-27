24119910c3a141e47dd3550630f157f87416a489
Davi Santos <morg.daviss@gmail.com>
2023-06-27 00:11:22 -0300

    Bootcamp first day

---
+++ br/section3/var.py
@@ -0,0 +1,12 @@
+name = 'Davi'
+last_name = 'Santos'
+age = 22
+birth_year = 2001
+height = 1.85
+
+print('Nome:', name)
+print('Sobrenome:', last_name)
+print('Idade:', age)
+print('Ano de nascimento', birth_year)
+print('É maior de idade?', age >= 18)
+print('Altura em metros:', height)
