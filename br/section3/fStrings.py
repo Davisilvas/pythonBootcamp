35e8497f13fb48de75555da9a3528a3af696e964
Davi Santos <morg.daviss@gmail.com>
2023-06-27 09:58:02 -0300

    Bootcamp 2 day

---
+++ br/section3/fStrings.py
@@ -0,0 +1,10 @@
+name = 'Davi Silva Santos'
+height = 1.851
+weight = 110.0
+imc = weight / height ** 2
+
+l1 = f'{name} tem {height:.2f} de altura'
+l2 = f'pesa {weight}KGs e seu IMC é: {imc:.2f}'
+
+print(l1)
+print(l2)
---
+++ br/section3/imc.py
@@ -0,0 +1,8 @@
+name = 'Davi Silva Santos'
+height = 1.85
+weight = 110.0
+imc = weight / height ** 2
+
+print(name + " tem " + str(height) + " de altura.")
+print("pesa", weight, "e seu IMC é:")
+print(imc)
